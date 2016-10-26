0fb700009e428ca35fd4813edcf3625d9b6a76bc
iven.wu <iven.wu@nordanglia.com>
2016-10-26 11:16:03 +0800

    ..

--- src/com/Play.java
+++ src/com/Play.java
@@ -9,17 +9,17 @@ public class Play {
 	private int packs; // 牌数
 
 	public void start() {
-		// 分牌
+		// 分玩家
 		Player player1 = new Player("No.1--");
 		Player player2 = new Player("No.2--");
 		Player player3 = new Player("No.3--");
-
-		Card card = new Card();
-		card.showLandlordCard();
 		LinkedList<Player> queue = new LinkedList<>();
 		queue.add(player1);
 		queue.add(player2);
 		queue.add(player3);
+
+		//分牌
+		Card card = new Card();
 
 		for (Player p : queue) {
 			card.distributional(p);
@@ -27,16 +27,23 @@ public class Play {
 
 		System.out.println(player1.getName() + "======================");
 		for (Integer i : player1.getHandCards()) {
-			System.out.println(i);
+			System.out.print(i+",");
 		}
+		System.out.println("");
 		System.out.println(player2.getName() + "======================");
 		for (Integer i : player2.getHandCards()) {
-			System.out.println(i);
+			System.out.print(i+",");
 		}
+		System.out.println("");
 		System.out.println(player3.getName() + "======================");
 		for (Integer i : player3.getHandCards()) {
-			System.out.println(i);
+			System.out.print(i+",");
 		}
+		System.out.println("");
+		//抢地主
+		System.out.println("");
+		System.out.println("============开始抢地主=============");
+		card.showLandlordCard();
 	}
 
 	public static void main(String[] args) {
